15a4e20f5429bacb7f652e2db52b46ab2dc3ce42
ADaneshS1 <daneshsudiono@gmail.com>
2025-10-04 21:26:56 +0700

    first assignment

---
+++ script/script.js
@@ -0,0 +1,39 @@
+const contacts = [
+  {
+    id: 1,
+    fullName: "Budi Setiawan",
+    phone: "+624233245578",
+    email: "budi233@example.com",
+    location: "Solo",
+  },
+  {
+    id: 2,
+    fullName: "Bambang Satria",
+    phone: "+62124435663",
+    email: "bambtria877@example.com",
+    location: "Lampung",
+  },
+  {
+    id: 3,
+    fullName: "Hasan Putra",
+    phone: "+629455377023",
+    email: "agusptrr1@example.com",
+    location: "Tangerang",
+  },
+  {
+    id: 4,
+    fullName: "Muhammad Abdul",
+    phone: "+6259943774",
+    email: "mhmabdul200@example.com",
+    location: "Jakarta",
+  },
+  {
+    id: 5,
+    fullName: "Pratama Faiz",
+    phone: "+6211449755",
+    email: "pratfaizzz22@example.com",
+    location: "Yogyakarta",
+  },
+];
+
+console.log(contacts);
