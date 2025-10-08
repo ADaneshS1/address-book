b86ddf4bd5249336535b4c1b4ec2aeb844c6b42e
ADaneshS1 <daneshsudiono@gmail.com>
2025-10-08 20:25:22 +0700

    add function,loop script

--- scripts/script.js
+++ scripts/script.js
@@ -2,38 +2,47 @@ const contacts = [
   {
     id: 1,
     fullName: "Budi Setiawan",
-    phone: "+624233245578",
+    phone: "+62-423-3245-578",
     email: "budi233@example.com",
     address: "Solo",
   },
   {
     id: 2,
     fullName: "Bambang Satria",
-    phone: "+62124435663",
+    phone: "+62-1244-356-963",
     email: "bambtria877@example.com",
     address: "Lampung",
   },
   {
     id: 3,
     fullName: "Hasan Putra",
-    phone: "+629455377023",
+    phone: "+62-9455-3770-023",
     email: "agusptrr1@example.com",
     address: "Tangerang",
   },
   {
     id: 4,
     fullName: "Muhammad Abdul",
-    phone: "+6259943774",
+    phone: "+62-9995-9943-774",
     email: "mhmabdul200@example.com",
     address: "Jakarta",
   },
   {
     id: 5,
     fullName: "Pratama Faiz",
-    phone: "+6211449755",
+    phone: "+62-1194-4975-995",
     email: "pratfaizzz22@example.com",
     address: "Yogyakarta",
   },
 ];
 
-console.log(contacts);
+function showContacts(contactList) {
+  for (let i = 0; i < contactList.length; i++) {
+    const contact = contactList[i];
+    console.log(
+      `${contact.fullName}, ${contact.phone}, ${contact.email}, ${contact.address}`
+    );
+  }
+}
+
+showContacts(contacts);
